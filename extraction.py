186d1f5ebd52ec96d2e78550421d681731abe15e
David Gillespie <david.gillespie@digital.cabinet-office.gov.uk>
2025-02-18 15:25:53 +0000

    bulk processing done for matt.

--- extraction.py
+++ extraction.py
@@ -1,10 +1,10 @@
 import logging
 import re
-from datetime import datetime
 
+import boto3
 import pandas as pd
 from dotenv import load_dotenv
-from llmbo import BatchInferer, ModelInput, StructuredBatchInferer
+from llmbo import ModelInput, StructuredBatchInferer
 from pydantic import BaseModel, Field
 
 logging.basicConfig(
@@ -16,7 +16,6 @@ logging.basicConfig(
     ],
 )
 
-load_dotenv()
 
 SYSTEM_PROMPT = """
 You are an expert in extracting skills from job descriptions and assessing their potential automation with GPT technology.
@@ -70,44 +69,94 @@ def load_department_jobs(department: str) -> pd.DataFrame:
     return department_data
 
 
-def create_job() -> None:
-    hmrc_data = load_department_jobs("HM Revenue and Customs")
+def skewer_department(department_name: str) -> str:
+    """
+    turn a department kebabcase
 
+    Args:
+        department_name (str): name of a department
+
+    Returns:
+        str: name of the department kebabcase
+    """
+    return re.sub(r"[^a-zA-Z0-9_]+", "-", department_name).lower().strip("-")
+
+
+def create_job(
+    department_data: pd.DataFrame,
+    department_name: str,
+    job_id: str,
+    session: boto3.Session,
+) -> str:
+    """
+    create a task extraction batch job
+
+    Args:
+        department_data (pd.DataFrame): a dataframe container vacancy_id and job_description
+        department_name (str): name of the department (must be kebabcase)
+        job_id (str): an id to associate with the job
+        session (boto3.Session): a session to use
+
+    Returns:
+        str: created job arn
+    """
     inputs = {
         f"vacancy_id={row.vacancy_id}": ModelInput(
             system=SYSTEM_PROMPT,
             messages=[{"role": "user", "content": prompt(row.job_description)}],
         )
-        for row in hmrc_data.itertuples()
+        for row in department_data.itertuples()
     }
 
     sbi = StructuredBatchInferer(
         model_name="us.anthropic.claude-3-5-sonnet-20241022-v2:0",
-        job_name=f"hmrc-task-extraction-{datetime.now().strftime('%Y%m%d-%H%M%S')}",
+        job_name=f"{department_name}-task-extraction-{job_id}",
         region="us-east-1",
         bucket_name="cddo-af-bedrock-batch-inference-us-east-1",
         role_arn="arn:aws:iam::992382722318:role/BatchInferenceRole",
         output_model=TaskOutput,  # type: ignore
+        session=session,
     )
 
     sbi.prepare_requests(inputs)
     sbi.push_requests_to_s3()
     sbi.create()
-    print(sbi.job_arn)
+    return sbi.job_arn
 
 
-def analyse_job():
-    sbi = StructuredBatchInferer.recover_structured_job(
-        job_arn="arn:aws:bedrock:us-east-1:992382722318:model-invocation-job/7whskxnh15zc",
-        region="us-east-1",
-        output_model=TaskOutput,
-    )
+# def analyse_job():
+#     sbi = StructuredBatchInferer.recover_structured_job(
+#         job_arn="arn:aws:bedrock:us-east-1:992382722318:model-invocation-job/7whskxnh15zc",
+#         region="us-east-1",
+#         output_model=TaskOutput,
+#     )
 
-    sbi.download_results()
-    sbi.load_results()
+#     sbi.download_results()
+#     sbi.load_results()
 
-    print(len(sbi.instances))
+#     print(len(sbi.instances))
+
+
+def convert_tasks_output_to_dataframe(tasks):
+    tasks_dict = {}
+    for item in tasks:
+        if item:
+            # Extract vacancy_id from the recordId string
+            vacancy_id = int(item["recordId"].split("=")[1])
+            # Get the tasks from the outputModel
+            if item["outputModel"]:
+                tasks = item["outputModel"].model_dump()["tasks"]
+                tasks_dict[vacancy_id] = tasks
+
+    return pd.DataFrame(
+        [
+            {"vacancy_id": key, **item}
+            for key, tasks in tasks_dict.items()
+            for item in tasks
+        ]
+    ).set_index("vacancy_id")
 
 
 if __name__ == "__main__":
-    analyse_job()
+    pass
+    # analyse_job()
